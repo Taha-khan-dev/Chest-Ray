30d3a96ca6eb0948816025d3746f107f81e00ed7
Taha Khan <157838999+Taha-khan-dev@users.noreply.github.com>
2025-04-05 20:25:50 +0500

    Fixes for html and flask and db

--- WebApp-PyFlask/Chest_Ray/__init__.py
+++ WebApp-PyFlask/Chest_Ray/__init__.py
@@ -29,17 +29,20 @@ def create_app(test_config=None):
 
     @app.route("/")
     def index():
-        return render_template("Login.html")
+        return render_template("login.html")
     
 
     @app.route("/check_login", methods = ("POST",))
     def check_login():
 
         try:
-
             connection = sqlite3.connect("CHESTRAYG6.db")
             connection.row_factory = sqlite3.Row
             cur = connection.cursor()
+
+            cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            print(cur.fetchall())
+
 
             username = request.form.get("username")
             password = request.form.get("password")
@@ -48,24 +51,24 @@ def create_app(test_config=None):
             if usertype == "admin":
                 cur.execute("SELECT * FROM users WHERE user_name = ? AND password = ?;", (username, password))
             elif usertype == "patient":
-                cur.execute("SELECT * FROM patients WHERE patient_name = ? AND password = ?;", (username, password))
+                cur.execute("SELECT * FROM patients WHERE patient_name = ? AND patient_password = ?;", (username, password))
             else:
                 return "404"
 
             user = cur.fetchone()
 
 
-            if user and  usertype == "admin":
-                session["user"] = user["user_name"]
-                session["user"] = usertype
+            if user and usertype == "admin":
+                session["username"] = user["user_name"]
+                session["userType"] = usertype
                 
                 connection.close()
 
                 return render_template("index.html")
             
-            elif user and  usertype == "patient":
-                session["user"] = user["patient_name"]
-                session["user"] = usertype
+            elif user and usertype == "patient":
+                session["username"] = user["patient_name"]
+                session["userType"] = usertype
 
                 connection.close()
 
